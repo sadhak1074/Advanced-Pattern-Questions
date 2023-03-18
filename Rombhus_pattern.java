d752fffd15c108dcbab8638fbb1ab4b21f31c30a
Sadhak Saini <sadhaksaini18@gmail.com>
2023-03-19 00:32:40 +0530

    Hollow Rhombus

--- Rombhus_pattern.java
+++ Rombhus_pattern.java
@@ -2,7 +2,7 @@ public class Rombhus_pattern{
     public static void main(String[] args){
         int n = 5;
         for (int i = 1; i <= n; i++){
-            for(int j = 1; j < i; j++){
+            for(int j = i; j <=1; j--){
                 System.out.print(" ");
             }
             for(int k = 1; k <= n; k++){
